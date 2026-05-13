e717c835317bb9ca41d206db41b3088ad975d00b
Extzy-1337 <anton.sazonov19.12@gmail.com>
2026-05-13 23:20:58 +0700

    Практическая работа №2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
